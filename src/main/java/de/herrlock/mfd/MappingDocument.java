886f3ffb3f2d00276650c84793ca35510b7a82d5
herrlock <herrlock@t-online.de>
2015-11-28 11:36:08 +0100

    immediately initialize the components

--- src/main/java/de/herrlock/mfd/MappingDocument.java
+++ src/main/java/de/herrlock/mfd/MappingDocument.java
@@ -1,12 +1,14 @@
 package de.herrlock.mfd;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
 import de.herrlock.mfd.elements.Component;
 import de.herrlock.mfd.util.Functions;
@@ -17,26 +19,19 @@ import de.herrlock.mfd.util.Functions;
  * @author HerrLock
  */
 public class MappingDocument {
-
     private static final Logger logger = LogManager.getLogger();
-    private final Document document;
+
     private final Element mappingNode;
-    private Iterable<Component> components;
+    private final List<Component> components;
 
     public MappingDocument( Document document ) {
-        this.document = document;
+        logger.entry();
         this.mappingNode = document.children().first();
-        if ( !"26".equals( this.mappingNode.attr( "version" ) ) ) {
-            logger.warn( "The document's version does not equal 26 (Mapforce 2015, Release 2), "
-                + "the result might be inaccurate or wrong." );
-        }
+        Elements select = this.mappingNode.select( "> component" );
+        this.components = Lists.transform( select, Functions.ELEMENT_TO_COMPONENT );
     }
 
-    public Iterable<Component> getComponents() {
-        if ( this.components == null ) {
-            Elements select = this.mappingNode.select( "component" );
-            this.components = Iterables.transform( select, Functions.ELEMENT_TO_COMPONENT );
-        }
+    public List<Component> getComponents() {
         return this.components;
     }
 }
