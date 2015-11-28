aa61795762e6227ba02a13202b3f56b491697d74
herrlock <herrlock@t-online.de>
2015-11-28 11:35:22 +0100

    update Main.java

--- src/main/java/de/herrlock/mfd/Main.java
+++ src/main/java/de/herrlock/mfd/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,23 +16,19 @@ import de.herrlock.mfd.elements.Component;
  * @author HerrLock
  */
 public class Main {
-    // public static final String FOLDER = "./MFTest";
-    // public static final String FILENAME = "Test";
-    public static final String FOLDER = ".";
-    public static final String FILENAME = "OrdersITX_to_IDocOrdersGen";
+    public static final String FOLDER = "./MFTest";
+    public static final String FILENAME = "Test";
+    // public static final String FILENAME = "OrdersITX_to_IDocOrdersGen";
 
     public static void main( String... args ) throws IOException {
-        parseMfd();
-    }
-
-    public static void parseMfd() throws IOException {
+        Document document;
         try ( InputStream in = Files.newInputStream( Paths.get( FOLDER, FILENAME + ".mfd" ) ) ) {
-            Document document = Jsoup.parse( in, "UTF-8", "", Parser.xmlParser() );
-            MappingDocument mappingDocument = new MappingDocument( document );
-            Iterable<Component> elements = mappingDocument.getComponents();
-            for ( Component component : elements ) {
-                System.out.println( component );
-            }
+            document = Jsoup.parse( in, "UTF-8", "", Parser.xmlParser() );
+        }
+        MappingDocument mappingDocument = new MappingDocument( document );
+        List<Component> elements = mappingDocument.getComponents();
+        for ( Component component : elements ) {
+            System.out.println( component );
         }
     }
 }
